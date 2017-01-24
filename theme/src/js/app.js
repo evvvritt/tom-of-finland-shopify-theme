c204cbc93af690e3f6c1c987eb26cb93cf3842ec
evvvritt <evv.info@gmail.com>
2017-01-24 18:18:26 +0100

    start

--- theme/src/js/app.js
+++ theme/src/js/app.js
@@ -25,7 +25,7 @@ app.artGrid = {
 
   _grid: $('.art-grid'),
   _items: $('.art-grid').children('.grid__item'),
-  _widths: ['20%', '30%', '30%', '30%', '40%'],
+  _widths: ['25%', '33.33%', '33.33%', '33.33%', '41.66%'],
 
   resizeItem: function ($item) {
     var widths = app.artGrid._widths;
@@ -44,154 +44,31 @@ app.artGrid = {
   },
 
   layout: function () {
-    // setup
-    var grid = app.artGrid._grid;
-    var items = app.artGrid._items;
-    var randNumb = app.utils.randomIntegerBt;
-
     if ( $(window).width() > 768 ) {
-        var placedItems = [];
-        var minX = 0;
-        var maxX = grid.width();
-        var itemX = randNumb(minX, maxX/12);
-        var itemY = 0;
-        var gridHeight = 0;
-        var gridBottomMargin = 100;
+      var grid = app.artGrid._grid;
+      var items = app.artGrid._items;
 
-        // prepare grid
-        this._grid.addClass('art-grid--enabled').removeClass('wrapper grid-uniform');
+      grid.imagesLoaded( function () {
+        app.utils.shuffleArray(items);
 
         // re-size items
         items.each( function () {
           app.artGrid.resizeItem( $(this) );
         });
 
-        // shuffle items
-        items = app.utils.shuffleArray(items);
+        //var Shuffle = window.shuffle;
+        //var element = document.querySelector('.art-grid');
 
-        // images loaded ?
-        grid.imagesLoaded( function () {
-          // position each item
-          items.each( function (index) { //console.log(index); //debugger;
-            var item = $(this);
-            var itemW = item.outerWidth(); //console.log(item.find('a').attr('href'));
-            var itemH = item.outerHeight();
+        //var shuffle = new Shuffle(element, {
+          //itemSelector: '.art-grid .grid__item',
+          //sizer: '.art-grid--sizer', //sizer // could also be a selector: '.my-sizer-element'
+        //});
 
-            if (placedItems.length > 0) {
-              for (var i = 0; i < placedItems.length; i++) {
-                //var thisPt = placedItems[i];
-                //var nextPt = placedItems[i+1];
-              };  
-            }
-
-            // position it!
-            item.css({left: itemX, top: itemY});
-
-            var itemBottomPos = {
-              x1: itemX,
-              x2: itemX+itemW,
-              y: itemY+itemH,
-              width: item
-            }
-            borderPts.push(itemBottomPos);
-
-            // update border pts
-            for (var i = 0; i < borderPts.length; i++) {
-              borderPts[i]
-            };
-
-            //
-            //
-            //
-            //
-            
-            var itemH = item.outerHeight();
-            var startNewRow = function () {
-              rows.push([]); // add new row
-              if (rows.length > 2 ) {
-                rows.shift();
-              }
-              // update x pos
-              itemX = 0; //randNumb(0, maxX/8);
-              //for (var i = 0; i < rows[0].length - 1; i++) {
-                //var firstRowItem = rows[0][0];
-                //var nextRowItem = rows[0][i+1];
-                //if ( nextRowItem.bottomEdge < firstRowItem.bottomEdge ) {
-                  //itemX = nextRowItem.leftEdge;
-                //}
-              //}
-              item.prepend('new row — ');
-            };
-            item.prepend(' - width: '+itemW);
-
-            // new row ?
-            if (itemX + itemW > maxX) {
-              startNewRow();
-            }
-            item.prepend(index);
-
-            // if not first row, get y
-            if (rows.length > 1) {
-              var y = false;
-              // loop through the items of previous row to see if fits
-              for (var i = 0; i < rows[0].length; i++) {
-                //var lastItemY = itemY;
-                var rowItem = rows[0][i];
-                var nextRowItem = rows[0][i+1];
-                var itemsInRow = rows[0].length - 1;
-                var isLastRowItem = i === itemsInRow;
-                var rightBoundary = isLastRowItem ? maxX : nextRowItem.leftEdge; // use maxX if last item in row
-                //minY = minY < rowItem.bottomEdge ? rowItem.bottomEdge : minY;
-
-                // skip this item in the row if incoming item starts past the edge
-                if ( itemX > rightBoundary ) {
-                  continue;
-                }
-
-                // fits ?
-                if ( itemW <= ( rightBoundary - itemX ) ) {
-                  y = rowItem.bottomEdge;
-                  break;
-                } else { // cant fit but maybe update xpos
-                  if (!isLastRowItem) itemX = rowItem.bottomEdge > nextRowItem.bottomEdge ? rowItem.rightEdge : itemX;
-                }
-              }
-              // useable Y ?
-              if (y === false) {
-                startNewRow();
-                itemY = rows[0][0].bottomEdge; // get bottom edge of first item in next row
-              } else {
-                itemY = y;
-              }
-            } else {
-              itemY = randNumb(1, 3) > 2 ? itemY + randNumb(0, 30) : itemY;
-            }
-
-            // position it!
-            item.css({left: itemX, top: itemY});
-
-            // store item in last row
-            var itemPos = {
-              width: itemW,
-              leftEdge: itemX,
-              rightEdge: itemX + itemW,
-              bottomEdge: itemY + itemH,
-            };
-            rows[rows.length-1].push(itemPos);
-
-            // update vars
-            itemX = itemX + itemW;
-            gridHeight = ( itemY + itemH ) > gridHeight ? itemY + itemH : gridHeight;
-          });
-
-          // update grid height
-          grid.css('height', gridHeight + gridBottomMargin);
-
-          // fade in
-          app.artGrid.showItems();
-        });
+        // fade in
+        app.artGrid.showItems();
+      });
     } else {
-      this.remove();
+      //this.remove();
     }
   },
 
